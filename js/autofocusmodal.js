9b7f6a404407eddd6c7571169d1ee2b63fcd915a
Kushal Sakurdeep <kushal.sakurdeep@gmail.com>
2017-09-27 21:30:39 +0400

    added modal for login and sign up

--- js/autofocusmodal.js
+++ js/autofocusmodal.js
@@ -1,3 +1,17 @@
 $('#myModal').on('shown.bs.modal', function () {
   $('#myInput').focus()
 })
+
+$('#myModal').modal('show');
+
+
+ window.alert = function(){};
+        var defaultCSS = document.getElementById('bootstrap-css');
+        function changeCSS(css){
+            if(css) $('head > link').filter(':first').replaceWith('<link rel="stylesheet" href="'+ css +'" type="text/css" />'); 
+            else $('head > link').filter(':first').replaceWith(defaultCSS); 
+        }
+        $( document ).ready(function() {
+          var iframe_height = parseInt($('html').height()); 
+          window.parent.postMessage( iframe_height, 'https://bootsnipp.com');
+        });
